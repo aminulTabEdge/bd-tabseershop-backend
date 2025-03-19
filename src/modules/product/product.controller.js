428613a232cdcb71e09320db12cd5f5a8d3bebc3
Aminul <aminultabedge@gmail.com>
2025-03-19 11:32:25 +0600

    Product delete by id functionalities added

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -3,45 +3,67 @@ const { productCollections } = require("../../utils/dbConnect");
 
 // Product post
 
-const postProduct = async (req,res) => {
-    const product = req.body
-    try {
-     const result = await productCollections.insertOne(product);
-     console.log(result)
-     res.status(201).send(result);
-   } catch (error) {
-     console.error(error);
-     res.status(400).send({ message: 'Something went wrong' });
-   }
-   }
+const postProduct = async (req, res) => {
+  const product = req.body;
+  try {
+    const result = await productCollections.insertOne(product);
+    console.log(result);
+    res.status(201).send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send({ message: "Something went wrong" });
+  }
+};
 
-
-   
 // Get all products
 const getAllProducts = async (req, res) => {
-    try {
-      const collection = await productCollections;
-      const result = await collection.find().toArray();
-  
-      res.status(200).send(result);
-    } catch (error) {
-      res.status(500).send({ error: error.message });
+  try {
+    const collection = await productCollections;
+    const { menu } = req.query;
+
+    let query = {};
+    if (menu) {
+      query.menu = { $regex: new RegExp(menu, "i") };
     }
-  };
+
+    const result = await collection.find(query).toArray();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 // Get  product by id
 const getProductById = async (req, res) => {
-    const {id} = req.params
-    try {
-        const query = {
-            _id : new ObjectId(id)
-        }
-      const collection = await productCollections 
-      const result = await collection.findOne(query)
-  
-      res.status(200).send(result);
-    } catch (error) {
-      res.status(500).send({ error: error.message });
-    }
-  };
+  const { id } = req.params;
+  try {
+    const query = {
+      _id: new ObjectId(id),
+    };
+    const collection = await productCollections;
+    const result = await collection.findOne(query);
 
-module.exports={postProduct, getAllProducts, getProductById}
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+// Deleyte product by id
+const deleteProductById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const query = { _id: new ObjectId(id) };
+    const result = await productCollections.deleteOne(query);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+module.exports = {
+  postProduct,
+  getAllProducts,
+  getProductById,
+  deleteProductById,
+};
--- src/modules/product/product.routes.js
+++ src/modules/product/product.routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const { postProduct, getAllProducts, getProductById } = require("./product.controller");
+const {
+  postProduct,
+  getAllProducts,
+  getProductById,
+  deleteProductById,
+} = require("./product.controller");
 const productRouter = express.Router();
 
 // ! Product post in the database
-productRouter.post('/products', postProduct )
-productRouter.get('/products', getAllProducts)
-productRouter.get('/products/:id', getProductById)
+productRouter.post("/products", postProduct);
+productRouter.get("/products", getAllProducts);
+productRouter.get("/products/:id", getProductById);
+productRouter.delete("/products/:id", deleteProductById);
 
-
-module.exports={productRouter} 
+module.exports = { productRouter };
